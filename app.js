136736fa8777ffc65993e9a9c7483e45c5e77206
CristianBrz <cristian.brz.83@gmail.com>
2024-07-02 22:57:21 -0300

    Agrego archivos estaticos

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require(`express`)
 const app = express()
 const port = 3000 || 8080 || process.env.PORT
 
+app.use(express.static(__dirname + '/public'))
+
 app.get("/", (req, res) => {
-  res.send(`<h1>Se respondio algo</h1>`)
+  res.sendFile(__dirname + `./public/index.html`)
 })
 
 app.listen(port, () => console.log(`Servidor funcionando en puerto ${port}`))
